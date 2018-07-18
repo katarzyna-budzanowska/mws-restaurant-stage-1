d2d895ac0de114411c908544fc074a5481fbbcfd
katarzyna-budzanowska <katarzyna.budzanowska@gmail.com>
2018-07-18 09:33:04 +0200

    Remove unnecesary console logs.

--- service_worker.js
+++ service_worker.js
@@ -23,14 +23,12 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(function(cache) {
-        console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
   );
 });
 
 self.addEventListener('fetch', function(event) {
-  console.log( 'fetch');
   event.respondWith(
     caches.match(event.request)
       .then(function(response) {
@@ -49,10 +47,8 @@ self.addEventListener('fetch', function(event) {
           function(response) {
             // Check if we received a valid response
             if(!response || response.status !== 200 || response.type !== 'basic') {
-              console.log( 'Cache hit!');
               return response;
             }
-            console.log( 'Cache miss!');
 
             // IMPORTANT: Clone the response. A response is a stream
             // and because we want the browser to consume the response
